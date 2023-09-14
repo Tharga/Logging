8e121c9cf5f6e1665cacbb75b0e16b09bd771380
Daniel Bohlin <daniel.bohlin@live.se>
2023-09-14 19:09:01 +0200

    provide automatic data

--- Logging.Sample.Web/Program.cs
+++ Logging.Sample.Web/Program.cs
@@ -2,41 +2,38 @@ using NLog;
 using Tharga.Logging.Logtail;
 using LogLevel = NLog.LogLevel;
 
-var pp = new LogtailLoggerProvider();
+namespace Logging.Sample.Web;
 
-var logger = LogManager.GetCurrentClassLogger();
-logger.Log(LogLevel.Info, "Initiating Logging.Sample.Web Server.");
-
-try
+public static class Program
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-    builder.Services.AddControllers();
-
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    public static void Main(string[] args)
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        var logger = LogManager.GetCurrentClassLogger();
+
+        try
+        {
+            logger.Log(LogLevel.Info, "Initiating Server.");
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{e.GetType().Name}: {e.Message} @{e.StackTrace}");
+            logger.Log(LogLevel.Fatal, e, $"Failed to start Server with message {e.Message}.");
+        }
     }
 
-    app.UseHttpsRedirection();
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddLogtailLogger();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Logging.Sample.Web.Startup>();
+            });
 
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-}
-catch (Exception e)
-{
-    Console.WriteLine($"{e.GetType().Name}: {e.Message} @{e.StackTrace}");
-    logger.Log(LogLevel.Fatal, e, $"Failed to start LeftBehind Server with message {e.Message}.");
+        return host;
+    }
 }
